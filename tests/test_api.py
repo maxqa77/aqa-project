e7210011acf3b4494a107dd743661659ec93b640
Максим Верехин <verehinqa@mail.ru>
2026-04-19 23:07:26 +0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,21 @@
 import pytest
+import allure
 from api.client import APIClient
+
+
+@allure.feature("ВАЛЕРА")
+@allure.story("Хватит Дрыхнуть")
+@allure.title("📄 Проверка получения списка постов (200 OK)")
+@allure.description("API должен вернуть список постов со статусом 200")
 
 @pytest.mark.api
 def test_get_posts(base_url):
-    client = APIClient(base_url)
-    response = client.get_posts()
-    assert response.status_code == 200
+
+    with allure.step("ВАЛЕРА"):
+        client = APIClient(base_url)
+
+    with allure.step("Отправляем запрос GET /posts"):
+        response = client.get_posts()
+
+    with allure.step("Проверяем статус код 200"):
+        assert response.status_code == 200
