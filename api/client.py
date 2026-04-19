dd4e4f0b44a8b89d00d618153cda2fc13baddb44
Максим Верехин <verehinqa@mail.ru>
2026-04-19 21:21:39 +0300

    Update client.py

--- api/client.py
+++ api/client.py
@@ -1,8 +1,17 @@
 import requests
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 class APIClient:
     def __init__(self, base_url):
         self.base_url = base_url
 
     def get_posts(self):
-        return requests.get(f"{self.base_url}/posts")
+        url = f"{self.base_url}/posts"
+        logger.info(f"GET {url}")
+
+        response = requests.get(url)
+
+        logger.info(f"Status: {response.status_code}")
+        return response
