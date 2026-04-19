aede32646e105575490753f8febf33e90dd01e79
Максим Верехин <verehinqa@mail.ru>
2026-04-19 21:26:41 +0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,8 @@
+import pytest
 from api.client import APIClient
 
+@pytest.mark.api
 def test_get_posts(base_url):
     client = APIClient(base_url)
     response = client.get_posts()
-
     assert response.status_code == 200
